10eaf5619e2fd8eeae7c31c4bc1c707b19c4f93b
renatompf <renatompf@hotmail.com>
2025-04-13 14:23:54 +0200

    set response content type and character encoding for validation errors in JSON format

--- src/main/java/io/renatofreire/servletspoc/utils/ValidationUtils.java
+++ src/main/java/io/renatofreire/servletspoc/utils/ValidationUtils.java
@@ -13,6 +13,8 @@ public class ValidationUtils {
     public static <T> boolean validateAndRespond(T input, Validator validator, ObjectMapper objectMapper, HttpServletResponse response) throws IOException {
         Set<ConstraintViolation<T>> violations = validator.validate(input);
         if (!violations.isEmpty()) {
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             objectMapper.writeValue(response.getWriter(), violations);
             return false;
